c7a803d9dab13e41d8bb5af6c009ca49ad50d4ef
dengdong <853315284@qq.com>
2017-10-12 17:55:24 +0800

    添加退格按钮事件，添加清空按钮事件

--- src/createKeyboard.js
+++ src/createKeyboard.js
@@ -82,44 +82,44 @@ function createKeyboard(screenText) {
         for (var i = 0, len_i = classNameList.length; i < len_i; i++) {
             switch (classNameList[i]) {
                 case 'nk-key-1':
-                    screenText += '1';
                     keyBtn = 'nk-key-1';
+                    screenText += '1';
                     break;
                 case 'nk-key-2':
-                    screenText += '2';
                     keyBtn = 'nk-key-2';
+                    screenText += '2';
                     break;
                 case 'nk-key-3':
-                    screenText += '3';
                     keyBtn = 'nk-key-3';
+                    screenText += '3';
                     break;
                 case 'nk-key-4':
-                    screenText += '4';
                     keyBtn = 'nk-key-4';
+                    screenText += '4';
                     break;
                 case 'nk-key-5':
-                    screenText += '5';
                     keyBtn = 'nk-key-5';
+                    screenText += '5';
                     break;
                 case 'nk-key-6':
-                    screenText += '6';
                     keyBtn = 'nk-key-6';
+                    screenText += '6';
                     break;
                 case 'nk-key-7':
-                    screenText += '7';
                     keyBtn = 'nk-key-7';
+                    screenText += '7';
                     break;
                 case 'nk-key-8':
-                    screenText += '8';
                     keyBtn = 'nk-key-8';
+                    screenText += '8';
                     break;
                 case 'nk-key-9':
-                    screenText += '9';
                     keyBtn = 'nk-key-9';
+                    screenText += '9';
                     break;
                 case 'nk-key-0':
-                    screenText += '0';
                     keyBtn = 'nk-key-0';
+                    screenText += '0';
                     break;
                 case 'nk-key-done':
                     keyBtn = 'nk-key-done';
@@ -133,12 +133,15 @@ function createKeyboard(screenText) {
                     break;
                 case 'nk-key-del':
                     keyBtn = 'nk-key-del';
+                    screenText = screenText.length > 0 ? screenText.substr(0, screenText.length - 1) : '';
                     break;
                 case 'nk-key-clear':
                     keyBtn = 'nk-key-clear';
+                    screenText = '';
                     break;
                 case 'nk-key-point':
                     keyBtn = 'nk-key-point';
+                    screenText += '.';
                     break;
                 case 'nk-key-negative':
                     keyBtn = 'nk-key-negative';
